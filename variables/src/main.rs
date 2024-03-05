dd5bfa0d6e4dad0287fafe032814b3156a28b8e7
siegkamgo <kamgosiegfried04@gmail.com>
2024-03-05 11:11:00 +0100

    variables, constants, & shadowing
    learned about variables, constants, and shadowing in Rust

---
+++ variables/src/main.rs
@@ -0,0 +1,33 @@
+// Variables
+
+// fn main() {
+//     let mut x = 5;
+//     println!("The value of x is: {x}");
+//     x = 6;
+//     println!("The value of x is: {x}");
+// }
+
+
+// Constants (Always immutable)
+
+// const HOURS_IN_A_YEAR: u32 = 365 * 24;
+// // fn main() {
+// //     println!("{HOURS_IN_A_YEAR}");
+    
+// // }
+
+
+// Shadowing
+
+fn main() {
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
